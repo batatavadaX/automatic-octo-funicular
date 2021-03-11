166fbda1940b6ad9a79017babeca38b72b1d0294
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-11 16:02:47 +0530

    Update shadowrun.py

--- shadowrun/shadowrun.py
+++ shadowrun/shadowrun.py
@@ -1,5 +1,5 @@
 from shadowrun import scheduler
 from .ninja import shadow_run
 
-def shadowrun(func=shadow_run, seconds=600):
+def add_job(func=shadow_run, seconds=600):
     scheduler.add_job(func, "interval", seconds=seconds
