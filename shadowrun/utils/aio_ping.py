58a57529a3f3a0a52f98f72a8a5e2c2fb4a2d0e9
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-11 14:17:19 +0530

    Update aio_ping.py

--- shadowrun/utils/aio_ping.py
+++ shadowrun/utils/aio_ping.py
@@ -3,8 +3,8 @@ from .get_host import host
 
 class run:
     @staticmethod
-    async def ping() -> int:
+    async def ping():
         async with aiohttp.request(
             "GET", host.host_()) as r:
-            return r.status
+            return int(r.status)
         
