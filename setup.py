413e1103cf148d29bb1090ccba4bdd0ee718b513
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-11 16:16:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as setup:
 
 setuptools.setup(
     name="shadowrun",
-    version="0.1.2",
+    version="0.11.2",
     author="Dhruv Maaniya (@midnightmadwalk)",
     author_email="midnightmadwalk@gmail.com",
     description="pyhton interaction with heroku website.",
