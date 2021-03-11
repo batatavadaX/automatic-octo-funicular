39e89d95957160659f936e0c79f3bb1e10e3382c
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-11 14:22:27 +0530

    Update scheduler.py

--- shadowrun/scheduler.py
+++ shadowrun/scheduler.py
@@ -4,5 +4,5 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 class schedule:
     def __init__() -> None:
         scheduler = AsyncIOScheduler()
-        scheduler.add_job(shadowrun, "interval", seconds=600)
+        scheduler.add_job(shadow_run, "interval", seconds=600)
         scheduler.start()
