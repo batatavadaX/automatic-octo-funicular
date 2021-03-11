f82ad2dc30038832554cff62d55c01a4b2a99368
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-11 16:40:33 +0530

    Update get_host.py

--- shadowrun/utils/get_host.py
+++ shadowrun/utils/get_host.py
@@ -4,6 +4,6 @@ import os
 
 def _host():
     __host__ = socket.getfqdn()
-    __port__ = os.environ.get("PORT", "6969")
+    __port__ = os.environ.get("PORT")
     __url__ = f"http://{__host__}:{__port__}"
     return __url__
