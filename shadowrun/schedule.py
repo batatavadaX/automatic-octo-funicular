d7af984c4ebe6321d3145c41e0cf81ef8733d1e5
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-11 17:37:24 +0530

    Update schedule.py

--- shadowrun/schedule.py
+++ shadowrun/schedule.py
@@ -1,1 +1,7 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+scheduler = AsyncIOScheduler()
+
+class shadowrun:
+    def add_job(ping, seconds=600):
+        scheduler.add_job(function, "interval", seconds=seconds)
