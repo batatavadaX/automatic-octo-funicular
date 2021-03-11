4f1ecd4f8b2ed269aa94217d1b2b304587f60686
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-11 17:37:38 +0530

    Update schedule.py

--- shadowrun/schedule.py
+++ shadowrun/schedule.py
@@ -4,4 +4,4 @@ scheduler = AsyncIOScheduler()
 
 class shadowrun:
     def add_job(ping, seconds=600):
-        scheduler.add_job(function, "interval", seconds=seconds)
+        scheduler.add_job(ping, "interval", seconds=seconds)
