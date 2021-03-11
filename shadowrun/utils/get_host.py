155953344e15d316de6ccf2f63d54db0dd2e4450
batatavadaX <75261987+batatavadaX@users.noreply.github.com>
2021-03-11 17:26:56 +0530

    Update get_host.py

--- shadowrun/utils/get_host.py
+++ shadowrun/utils/get_host.py
@@ -5,7 +5,7 @@ import os
 def _host():
     __host__ = socket.getfqdn()
     __port__ = os.environ.get("PORT", 6969)
-    if __port__ = 6969:
+    if __port__ == 6969:
         print("no web process running")
     return
     __url__ = f"http://{__host__}:{__port__}"
